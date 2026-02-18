4492d7a22ad4575c3dc1ebc82b43ce298920de95
Yash Patil <ya88287946@gmail.com>
2026-02-18 13:06:16 +0530

    Separate file by using express

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
+// <----------app.js file--------->
+
 const express = require('express');
-const app =  express();
+const app = express();
 
-app.get('/orders', (req,res)=>{
-    res.send('Here is the list of all orders');
+const orderRouters = require('./routes/users');
+
+
+app.use('/users', orderRouters);
+
+app.listen(3000, ()=>{console.log('Server is running at port 3000')});
+
+
+//<-----------users.js file---------->
+
+const express = require('express');
+const routes = express.Router();
+
+routes.get('/',(req,res)=>{
+    res.send('User list');
 })
 
-app.post('/orders',(req,res)=>{
-    res.send('A new order has been created.');
+routes.post('/',(req,res)=>{
+    res.send('Update user list');
 })
 
-app.get('/users', (req,res)=>{
-    res.send('Here is the list of all users.');
-})
-
-app.post('/users',(req,res)=>{
-    res.send('A new user has been added');
-})
-
-
-
-app.listen(3000,()=>{console.log('server is running on http://localhost:3000')});
+module.exports= routes;
 
